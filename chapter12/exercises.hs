337bd3418c60d39e0732d65c7cf3bf674affccf5
peano88 <ilpeano@gmail.com>
2021-03-05 00:01:44 +0100

    chapter 12

--- chapter12/exercises.hs
+++ chapter12/exercises.hs
@@ -59,3 +59,29 @@ instance Monad Expr where
     (Val x) >>= _ = Val x
     (Add l r) >>= f = Add (l >>= f) (r>>= f)
 
+type State = Int
+
+newtype ST a = S (State -> (a,State))
+
+app :: ST a -> State -> (a,State)
+app (S st) = st
+
+instance Functor ST where
+    -- fmap :: (a -> b) -> ST a -> ST b
+    fmap g st = do
+        x <- st
+        return (g x) -- aka st >>= \x -> return g x
+
+instance Applicative ST where
+    -- pure :: a -> ST a
+    pure x = S (\s -> (x,s))
+    
+    -- (<*>) :: ST (a -> b) -> ST a -> ST b
+    stf <*> stx = do
+        f <- stf
+        x <- stx
+        return (f x) -- aka stf >>= \f -> stx >> \x -> return (f x)
+
+instance Monad ST where
+    -- (>>=) :: ST a -> (a -> ST b) -> ST b
+    st >>= f = S (\s ->let (x,s') = app st s in app (f x) s')
